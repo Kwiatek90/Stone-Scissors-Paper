af47b5bf5c559ab968903274c4661f9f3bee9fd5
kwiatek90 <mateusz.kwiat9@gmail.com>
2023-06-26 16:15:18 +0200

    Update

--- stone_scissor_paper.py
+++ stone_scissor_paper.py
@@ -9,6 +9,34 @@
 #   [21], [22], [23]
 #   [31], [32], [33]
 
-board = {
-    
-        }
+#wyswietlic tablice
+#dodawac wartosc
+#tworzyc wygrane
+#dodac komputer
+
+
+def show_board(board):
+        for i in range(3):
+                print("\t")
+                for j in range(3):
+                        print(board[i][j], end="\t")
+                
+        
+        
+
+
+
+board = [["_","_","_"], ["_","_","_"], ["_","_","_"]]
+
+
+while True:
+        column = input("Enter column(1-3): ")
+        line = input("Enter line(1-3): ")
+        if line == "Exit" or column == "Exit":
+                break
+        line = int(line) - 1
+        column = int(column) - 1
+        board[line][column] = "X"
+        show_board(board)
+
+
