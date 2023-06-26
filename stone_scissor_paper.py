c80904781eaddf592a1717b9b6e3d5a08dedd363
kwiatek90 <mateusz.kwiat9@gmail.com>
2023-06-26 15:34:27 +0200

    Create rep

---
+++ stone_scissor_paper.py
@@ -0,0 +1,14 @@
+#Stone_scissor_paper
+#W oby przypadkach musi być również możliwość gry z komputerem.
+
+#W przypadku 1️⃣, jest to trywialne, bo to tylko loswanie jednego z 3 elementów
+#W przypadku 2️⃣, zrób to najprościej jak się da i po prostu losuj co się pojawi na 
+#dowolnym polu, nie staraj się teraz tworzyć, algorytmu "myślenia" analizującego planszę.
+
+#   [11] , [12], [13]
+#   [21], [22], [23]
+#   [31], [32], [33]
+
+board = {
+    
+        }
